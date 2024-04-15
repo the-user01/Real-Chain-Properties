fe0abbc81d691b124468d175f6809e6e7686ae8e
the-user01 <sakibcom02@gmail.com>
2024-04-16 03:58:03 +0600

    Updated firebase folder

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDmeWnSg4WfiQIBUjse72ig1loU_wAUIm8",
-  authDomain: "react-real-estate-project.firebaseapp.com",
-  projectId: "react-real-estate-project",
-  storageBucket: "react-real-estate-project.appspot.com",
-  messagingSenderId: "826381393874",
-  appId: "1:826381393874:web:74feeabcf2e77a8730720b"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
